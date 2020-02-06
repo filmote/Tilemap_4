0122aadb517a84fbd945c09616068faa5c385a54
simonjholmes <simon@bloggingadeadhorse.com>
2020-02-06 14:28:02 +1100

    Updates

--- images/Images.h
+++ images/Images.h
@@ -13,10 +13,5 @@ namespace Images {
     #include "Water.h"
     #include "Player.h"
     #include "Enemy.h"
-    
-    Tilemap::Tile green16 = { &Green[2], Green[0], Green[1] };
-    Tilemap::Tile tree16 = { &Tree[2], Tree[0], Tree[1] };
-    Tilemap::Tile grass16 = { &Grass[2], Grass[0], Grass[1] };
-    Tilemap::Tile water16 = { &Water[2], Water[0], Water[1] };
 
 }
